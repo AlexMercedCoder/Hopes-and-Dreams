456deebf6e09242117869ad61ccc6430c951f4e0
Alex Merced <alex@alexmerced.dev>
2021-04-12 12:09:20 -0400

    full crud on dreams

--- routes/dreams.js
+++ routes/dreams.js
@@ -14,12 +14,41 @@ router.use(auth);
 ///////////////////////////////
 // Router Routes
 ////////////////////////////////
-router.get("/", (req, res) => {
-  res.render("home");
+router.get("/", async (req, res) => {
+  const dreams = await Dream.find({ user: req.session.user.id });
+  console.log(dreams);
+  res.render("dreams/index", {
+    dreams,
+  });
 });
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+router.post("/", async (req, res) => {
+  req.body.user = req.session.user.id;
+  await Dream.create(req.body);
+  res.redirect("/dreams/");
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const dream = await Dream.findOne({ _id: id, user: req.session.user.id });
+  console.log(dream)
+  if (dream) {
+    res.render("dreams/show", {
+      dream,
+    });
+  } else {
+    res.status(400).json({ error: "No Dream of This ID for this user" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  await Dream.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect("/dreams");
+});
+
+router.delete("/:id", async (req, res) => {
+  await Dream.findByIdAndRemove(req.params.id);
+  res.redirect("/dreams");
 });
 
 ///////////////////////////////
